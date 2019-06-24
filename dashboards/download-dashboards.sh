63020a94d36f545c0ae02a8b7e3b03427919b09c
David Salek <salekd@gmail.com>
2019-06-24 12:19:52 +0200

    remove metadata

--- dashboards/download-dashboards.sh
+++ dashboards/download-dashboards.sh
@@ -6,5 +6,6 @@ for uid in $uids
 do
   curl -s https://grafana.sensemakersams.org/api/dashboards/uid/$uid -u "admin:$PASSWORD" | jq . > dashboard-$uid.json
   jq '.dashboard.id=null' dashboard-$uid.json | sponge dashboard-$uid.json
+  jq 'del(.meta)' dashboard-$uid.json | sponge dashboard-$uid.json
   echo dashboard-$uid.json $(jq .'dashboard.title' dashboard-$uid.json)
 done
